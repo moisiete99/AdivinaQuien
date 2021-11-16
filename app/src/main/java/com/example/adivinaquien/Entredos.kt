cf6673c3c8ba7934bb1fb1aa3d1de324435ac82e
moy <moy_rub@hotmail.com>
2021-11-16 16:58:29 -0600

    preparando imagenes

---
+++ app/src/main/java/com/example/adivinaquien/Entredos.kt
@@ -0,0 +1,19 @@
+package com.example.adivinaquien
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
+class Entredos : Activity() {
+
+    //array de img's
+    /*val img01,img02,img03,img04,img05,img06,img07,img08,
+    img09,img03,img03,img03,img03,img03,img03,img03,
+    img03,img03,img03,img03,img03,img03,img03,img03,*/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
