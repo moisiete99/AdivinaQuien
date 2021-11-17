be6944d06d2ff779423784d2fe8f2e15a3bfc89a
moy <moy_rub@hotmail.com>
2021-11-16 19:49:36 -0600

    corregir llamado de Activity

--- app/src/main/java/com/example/adivinaquien/Entredos.kt
+++ app/src/main/java/com/example/adivinaquien/Entredos.kt
@@ -2,18 +2,50 @@ package com.example.adivinaquien
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Button
+import android.os.PersistableBundle
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 
-class Entredos : Activity() {
+class Entredos : AppCompatActivity() {
 
-    //array de img's
-    /*val img01,img02,img03,img04,img05,img06,img07,img08,
-    img09,img03,img03,img03,img03,img03,img03,img03,
-    img03,img03,img03,img03,img03,img03,img03,img03,*/
+    //img's
+    var img01 : ImageButton = findViewById(R.id.btn01)
+    var img02 : ImageButton = findViewById(R.id.btn02)
+    var img03 : ImageButton = findViewById(R.id.btn03)
+    var img04 : ImageButton = findViewById(R.id.btn04)
+    var img05 : ImageButton = findViewById(R.id.btn05)
+    var img06 : ImageButton = findViewById(R.id.btn06)
+    var img07 : ImageButton = findViewById(R.id.btn07)
+    var img08 : ImageButton = findViewById(R.id.btn08)
+    var img09 : ImageButton = findViewById(R.id.btn09)
+    var img10 : ImageButton = findViewById(R.id.btn10)
+    var img11 : ImageButton = findViewById(R.id.btn11)
+    var img12 : ImageButton = findViewById(R.id.btn12)
+    var img13 : ImageButton = findViewById(R.id.btn13)
+    var img14 : ImageButton = findViewById(R.id.btn14)
+    var img15 : ImageButton = findViewById(R.id.btn15)
+    var img16 : ImageButton = findViewById(R.id.btn16)
+    var img17 : ImageButton = findViewById(R.id.btn17)
+    var img18 : ImageButton = findViewById(R.id.btn18)
+    var img19 : ImageButton = findViewById(R.id.btn19)
+    var img20 : ImageButton = findViewById(R.id.btn20)
+    var img21 : ImageButton = findViewById(R.id.btn21)
+    var img22 : ImageButton = findViewById(R.id.btn22)
+    var img23 : ImageButton = findViewById(R.id.btn23)
+    var img24 : ImageButton = findViewById(R.id.btn24)
+    /*img01,img02,img03,img04,img05,img06,img07,img08,
+    img09,img10,img11,img12,img13,img14,img15,img16,
+    img17,img18,img19,img20,img21,img22,img23,img24 : Image*/
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    //var tablero : ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.entredos)
+
+        val tablero = arrayOf<ImageButton>(
+            img01,img02,img03,img04,img05,img06,img07,img08,
+            img09,img10,img11,img12,img13,img14,img15,img16,
+            img17,img18,img19,img20,img21,img22,img23,img24)
     }
 }
--- app/src/main/java/com/example/adivinaquien/MainActivity.kt
+++ app/src/main/java/com/example/adivinaquien/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.adivinaquien
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnvs2 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.entredos)
+
+        /*btnvs2 = findViewById(R.id.btnVS2)
+        btnvs2.setOnClickListener{
+            println("Iniciando 2vs2")
+            iniciarJuego2()
+        }*/
+    }
+
+    fun iniciarJuego2(){
+        val i = Intent(this,Entredos::class.java)
+        startActivity(i)
     }
 }
