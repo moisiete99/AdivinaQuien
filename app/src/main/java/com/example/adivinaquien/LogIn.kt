832b278acd16f8868b2708f346af6aa0b9235e8d
moy <moy_rub@hotmail.com>
2021-11-23 17:12:36 -0600

    llamando iniciar en login y signup xml OK

--- app/src/main/java/com/example/adivinaquien/LogIn.kt
+++ app/src/main/java/com/example/adivinaquien/LogIn.kt
@@ -1,11 +1,29 @@
 package com.example.adivinaquien
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LogIn : AppCompatActivity() {
+
+    lateinit var edtEmail:EditText
+    lateinit var edtPassword:EditText
+    lateinit var btnLogin: Button
+    lateinit var btnSignup: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        edtEmail=findViewById(R.id.edt_email)
+        edtPassword=findViewById(R.id.edt_password)
+        btnLogin=findViewById(R.id.btn_login)
+        btnSignup=findViewById(R.id.btn_signup)
+
+        btnSignup.setOnClickListener{
+            val i = Intent(this, SignUp::class.java)
+            startActivity(i)
+        }
     }
 }
