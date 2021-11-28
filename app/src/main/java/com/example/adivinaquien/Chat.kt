305920086e2ef7f48916f15f41e1e89968348acf
moy <moy_rub@hotmail.com>
2021-11-28 12:07:01 -0600

    lista de Chat OK

--- app/src/main/java/com/example/adivinaquien/Chat.kt
+++ app/src/main/java/com/example/adivinaquien/Chat.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class Chat : AppCompatActivity() {
 
@@ -14,17 +16,42 @@ class Chat : AppCompatActivity() {
     lateinit var userList: ArrayList<User>
     lateinit var adapter: UserAdapter
     lateinit var mAuth: FirebaseAuth
+    lateinit var mDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-        mAuth = FirebaseAuth.getInstance()
+        mAuth = FirebaseAuth.getInstance() //intancia para autentificacion
+        mDbRef = FirebaseDatabase.getInstance().getReference() //instancia para BD
 
         userList = ArrayList()
         adapter = UserAdapter(this, userList)
 
-        userRecyclerView = findViewById(R.id.userRecyclerView)
+        userRecyclerView = findViewById(R.id.userRecyclerView) //1:19 video
+
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+        userRecyclerView.adapter = adapter
+
+        mDbRef.child("user").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                userList.clear() //limpiamos la lista de usuarios
+                for (postSnapshot in snapshot.children) { //mostramos usuarios
+                    val currentUser = postSnapshot.getValue(User::class.java)
+
+                    if (mAuth.currentUser?.uid != currentUser?.uid) { //no mostramos nuestro propio usuario
+                        userList.add(currentUser!!)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
